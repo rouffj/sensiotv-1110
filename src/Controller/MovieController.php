7f88a0779fafe822f278f4cc4cfc54128612b26b
Joseph Rouff <rouffj@gmail.com>
2021-10-11 16:40:26 +0200

    Exo about template inheritance + dynamize /search

--- src/Controller/MovieController.php
+++ src/Controller/MovieController.php
@@ -3,8 +3,10 @@
 namespace App\Controller;
 
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
+use App\OmdbApi;
 
 /**
  * @Route("/movie", name="movie_")
@@ -30,9 +32,15 @@ class MovieController extends AbstractController
     /**
      * @Route("/search", name="search")
      */
-    public function search(): Response
+    public function search(OmdbApi $omdbApi, Request $request): Response
     {
+        $keyword = $request->query->get('keyword', 'Harry Potter');
+        $movies = $omdbApi->requestAllBySearch($keyword);
+        dump($movies);
+
         return $this->render('movie/search.html.twig', [
+            'movies' => $movies['Search'],
+            'keyword' => $keyword,
         ]);
     }
 }
