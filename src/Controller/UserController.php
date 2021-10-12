a32f6b5ae02c28910c436d6bf88069725a9b80d7
Joseph Rouff <rouffj@gmail.com>
2021-10-12 11:14:40 +0200

    Finish exo about twig extends

--- src/Controller/UserController.php
+++ src/Controller/UserController.php
@@ -3,17 +3,29 @@
 namespace App\Controller;
 
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
+use App\Form\UserType;
 
 class UserController extends AbstractController
 {
     /**
      * @Route("/register", name="register")
      */
-    public function register(): Response
+    public function register(Request $request): Response
     {
-        return $this->render('user/register.html.twig');
+        $form = $this->createForm(UserType::class);
+
+        if ($form->handleRequest($request)->isSubmitted() && $form->isValid()) {
+            $entity = $form->getData();
+            dump($entity);
+            // TODO: Your entity is ready to be inserted into DB
+        }
+
+        return $this->render('user/register.html.twig', [
+            'form' => $form->createView(),
+        ]);
     }
 
     /**
